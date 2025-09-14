bdf31b3cba6852e3d700252c14904e2b2173e01d
himn1 <hello@artofcode.co>
2025-09-14 05:53:22 +0200

    refactoring

--- src/start/init.js
+++ src/start/init.js
@@ -7,13 +7,13 @@ export const initializeMatches = (elementToNode, componentClasses) => {
 
       const instance = new ComponentClass();
       instance.element = node.element;
-      const sanitizedNode = { ...node };
-      delete sanitizedNode.name;
-      delete sanitizedNode.element;
-      instance.node = sanitizedNode;
+      const { parent, children } = { ...node };
+      instance.node = { parent, children };
       instance.componentId = generateComponentId();
+
       node.element.setAttribute("data-component-id", instance.componentId);
       node.element.instance = instance;
+
       if (typeof instance.connect === "function") instance.connect();
     }
   }
