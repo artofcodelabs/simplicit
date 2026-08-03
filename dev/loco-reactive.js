c2dedbc4d2be3e7b771ee312b3394fc8a0c1187b
himn1 <hello@artofcode.co>
2026-08-03 06:18:24 +0200

    name vs identity

--- dev/loco-reactive.js
+++ dev/loco-reactive.js
@@ -2,6 +2,10 @@ import { Reactive } from "Simplicit";
 
 export const locoReactive = (Base) =>
   class extends Reactive(Base) {
+    static getIdentity() {
+      return this.identity ?? this.name;
+    }
+
     update(partial = {}) {
       for (const [key, val] of Object.entries(partial))
         this.assignAttr(key, val);
