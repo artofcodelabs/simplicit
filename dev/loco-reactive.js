00369d3ca3dcb393f5111f83cfc4f22d8984a80f
himn1 <hello@artofcode.co>
2026-08-04 05:44:06 +0200

    integration with loco-js-model

--- dev/loco-reactive.js
+++ dev/loco-reactive.js
@@ -6,13 +6,33 @@ export const locoReactive = (Base) =>
       return this.identity ?? this.name;
     }
 
-    async save(...args) {
+    static async all(...args) {
+      const records = await super.all(...args);
+      return this.load(records);
+    }
+
+    async save(partial = {}) {
       const isCreate = this.id == null;
-      const resp = await super.save(...args);
-      if (isCreate && resp.success) {
-        if (resp.id != null) this.id = resp.id;
+      const target = isCreate ? this : this.clone();
+      for (const [key, val] of Object.entries(partial))
+        target.assignAttr(key, val);
+      if (target.isInvalid()) return { success: false, errors: target.errors };
+
+      const resp = await Base.prototype.save.call(target);
+      if (!resp.success) return resp;
+
+      if (isCreate) {
+        this.id = resp.id;
         this.constructor.add(this);
+      } else {
+        this.update(partial);
       }
+      return resp;
+    }
+
+    async delete(...args) {
+      const resp = await super.delete(...args);
+      if (resp.success) this.del();
       return resp;
     }
 
